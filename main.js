31fa2f032da48c07793a46f9985ef2fae7388866
Sergei <tustanovskiis@gmail.com>
2020-01-24 13:20:31 +0200

    signIn

--- main.js
+++ main.js
@@ -1,4 +1,80 @@
-//left sliders
+  history.replaceState(null, null, ' ');
+  // ============================burger menu===========================
+let burgMenu = document.getElementById('burgerMenu'),
+	header = document.querySelector('.header'),
+	headerNavBurger = document.querySelector('.headerNavBurger'),
+	middleLine = document.querySelector('.middleLine'),
+	topLine = document.querySelector('.topLine'),
+	bottomLine = document.querySelector('.bottomLine');
+
+window.addEventListener("resize", function(){
+	if (innerWidth > 1365 ) headerNavBurger.style.display ='none',
+
+	middleLine.style = `
+		display:block;
+	`,
+
+	topLine.style =`
+		top: 20%;
+    	left: 50%;
+		transform: translate(-50%, -20%);
+	`,
+
+	bottomLine.style =`
+		bottom: 20%;
+    	left: 50%;
+		transform: translate(-50%, -20%) ;
+	` ;
+});
+function closeMenu (){
+	middleLine.style = `
+		display:block;
+	`
+	topLine.style =`
+		top: 20%;
+    	left: 50%;
+		transform: translate(-50%, -20%);
+	`
+	bottomLine.style =`
+		bottom: 20%;
+    	left: 50%;
+		transform: translate(-50%, -20%) ;
+	`
+	headerNavBurger.style = `
+		right:-50%;
+	`
+}
+function openMenu(){
+	middleLine.style = `
+		display:none;
+	`
+	topLine.style =`
+		top: 50%;
+    	left: 50%;
+		transform: translate(-50%, -50%) rotate(-45deg);
+	`
+	bottomLine.style =`
+		top: 50%;
+    	left: 50%;
+		transform: translate(-50%, -50%) rotate(45deg);
+	`
+	headerNavBurger.style = `
+		right:5%;
+	`
+}
+
+burgMenu.addEventListener("click",(
+		function (){
+			let statusMenu = false
+			return function(event){
+				statusMenu ? closeMenu() : openMenu ()
+				statusMenu = !statusMenu
+			}
+		}
+	)()
+); 
+
+//=======================left sliders=========================
 
 let slideIndex = 1 ,
 	slides = document.querySelectorAll('.sliderItem'),
@@ -33,7 +109,7 @@ dotsWrap.onclick = function (event) {
 		}
 	}
 }
-//right sliders
+//=======================right sliders================================
 let slideIndex2 = 1 ,
 	slides2 = document.querySelectorAll('.sliderItem2'),
 	dotsWrap2 = document.querySelector('.sliderDots2'),
@@ -67,18 +143,24 @@ dotsWrap2.onclick = function (event) {
 		}
 	}
 }
-
+//===========================open popup===========================
 let openPopUp = document.getElementById('join');
+let openPopUp2 = document.getElementById('join2')
 let closePopUp = document.getElementById('closePopUp');
 let popUpDiv = document.getElementById('popUpBg');
 openPopUp.onclick = function(event) {
-	popUpBg.style.display = 'block';
+	popUpDiv.style.display = 'block';
 }
 closePopUp.onclick = function (event) {
-	popUpBg.style.display = 'none';
+	popUpDiv.style.display = 'none';
 }
-
-//====================form
+openPopUp2.onclick = function(event) {
+	popUpDiv.style.display = 'block';
+}
+closePopUp.onclick = function (event) {
+	popUpDiv.style.display = 'none';
+}
+//====================form========================================
 const userInfo = {};
 
 let name = document.getElementById('userName'),
@@ -150,4 +232,69 @@ submit.onclick = function (event) {
 	})
 		.then(response => response.json())
 			.then(response => console.log(response))
+}
+
+//================================= sign up ===========================
+let signUp = document.getElementById('signUp'),
+	signUp2 = document.getElementById('signUp2'),
+	popUpBg2 = document.getElementById('popUpBg2'),
+	closePopUp2 = document.querySelector('.closePopUp2')
+
+signUp.onclick = function(event) {
+	popUpBg2.style.display = 'block';
+}
+
+signUp2.onclick = function(event) {
+	popUpBg2.style.display = 'block';
+}
+
+closePopUp2.onclick = function(event) {
+	popUpBg2.style.display = 'none'; 
+}
+//=======================================form sign up==================================
+
+const userInfoSignUp = {}
+
+let login = document.getElementById('login'),
+	passwordUp1 = document.getElementById('pass1'),
+	passwordUp2 = document.getElementById('pass2'),
+	submit2 = document.getElementById('submit2');
+
+login.oninput = function (event) {
+		event.target.value.length > 8 || event.target.value.length === 0?
+			 event.target.style.color ='red' :
+				 event.target.style.color = 'white'
+};	
+login.onchange = function (event) {
+	userInfoSignUp.login = event.target.value
+};
+passwordUp1.oninput = function ( event ) {
+    let pass = event.target.value
+    event.target.valid = pass.length > 6 && !!pass.match ( /\d/ ) && !!pass.match ( /\D/ )
+    event.target.style.color = event.target.valid ? "green" : "red"
+    
+    passwordUp2.disabled = !event.target.valid
+}
+
+passwordUp2.oninput = function ( event ) {
+    event.target.valid = event.target.value === passwordUp1.value
+    event.target.style.color = event.target.valid ? "green" : "red"
+}
+
+passwordUp2.onchange = function ( event ) {
+    event.target.valid ?
+       	userInfoSignUp.password = Sha256.hash ( event.target.value ) : null
+}
+submit2.onclick = function (event) {
+	fetch(`https://garevna-rest-api.glitch.me/user/${userInfoSignUp.login}`,{
+		method:"POST",
+		headers:{
+			"Content-Type":"application/json"
+		},
+		body: JSON.stringify(userInfoSignUp)
+	})
+		.then(response => response.json())
+			.then(response => console.log(response))
+
+	popUpBg2.style.display ='none';		
 }
